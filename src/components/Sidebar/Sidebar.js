966550ba5a59a3f88bc80faa2a47e980e3e84808
Emmanuel Castillo <emmanuel@integrass.com>
2019-06-06 16:52:08 -0700

    Sidebar component initial commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Sidebar = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">THE LOGO</a></div>
+            <div>
+                <ul>
+                    <li><a href="/">Something about me</a></li>
+                    <li><a href="/">My projects</a></li>
+                    <li><a href="/">My fav books</a></li>
+                    <li><a href="/">..more</a></li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>    
+);
+
+export default Sidebar;
